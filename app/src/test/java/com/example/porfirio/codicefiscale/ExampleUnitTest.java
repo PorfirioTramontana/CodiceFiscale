eb304fb5884304ab6537877e48109ddced1a2f7e
Porfirio Tramontana <porfirio.tramontana@gmail.com>
2020-01-28 16:17:33 +0100

    Aggiunto nuovo servizio di reverse geocoding con test

--- app/src/test/java/com/example/porfirio/codicefiscale/ExampleUnitTest.java
+++ app/src/test/java/com/example/porfirio/codicefiscale/ExampleUnitTest.java
@@ -5,7 +5,9 @@ import com.example.porfirio.codicefiscale.engine.ReverseGeocoding;
 
 import org.junit.Test;
 
+import static java.lang.String.valueOf;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -24,4 +26,20 @@ public class ExampleUnitTest {
         String s = ReverseGeocoding.getCity("40.843107", "14.226497", cc);
         assertEquals("Napoli", s);
     }
+
+    @Test
+    public void provaCitta2() {
+        CitiesCodes cc = new CitiesCodes();
+        for (int i = 30; i < 50; i += 1) {
+            for (int j = 4; j < 24; j += 1) {
+                String s = ReverseGeocoding.getCity(valueOf(i), valueOf(j), cc);
+                System.err.println(i);
+                System.err.println(j);
+                System.err.println(s);
+                assertNotNull(s, s);
+            }
+        }
+    }
+
+
 }
